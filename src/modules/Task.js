49dbf05859b5c72cf3f907e0bde9faa8506df953
gabicilfone <cilfonegabriel@gmail.com>
2022-12-15 16:08:31 -0300

    fix linters error

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -26,7 +26,7 @@ export default class Task {
   get getCompleted() {
     return this.completed;
   }
-  
+
   get getIndex() {
     return this.index;
   }
--- src/modules/ToDoList.js
+++ src/modules/ToDoList.js
@@ -3,22 +3,28 @@ export default class ToDoList {
   constructor(taskList = []) {
     this.taskList = taskList;
   }
+
   get getTaskList() {
     return this.taskList;
   }
+
   set setTaskList(newTaskList = []) {
     if (!newTaskList) return;
     this.taskList = newTaskList;
   }
+
   getLength() {
     return this.taskList.length;
   }
+
   addTask(task) {
     this.taskList.push(task);
   }
+
   removeTask(index) {
     this.taskList.splice(index, 1);
   }
+
   updateTaskIndex() {
     this.sortTasks();
     for (let i = 0; i < this.taskList.length; i += 1) {
